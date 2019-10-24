2e9de3f42620109ee029d6bd655b666573c66001
fengchen <fengchen@meili-inc.com>
2019-10-24 17:10:11 +0800

    F001

--- foo/foo.go
+++ foo/foo.go
@@ -2,9 +2,12 @@ package foo
 
 import "fmt"
 
-//在  1.0.0 版本有效
+//在  v1.0.0 版本有效
 func Foo()  {
 	fmt.Println("foo foo foo ...")
 }
 
-
+//在 v1.1.0 版本有效
+func Foo1()  {
+	fmt.Println("foo1 foo1 foo1 ...")
+}
--- main.go
+++ main.go
@@ -4,4 +4,6 @@ import "github.com/wshlovercn/moda/foo"
 
 func main()  {
 	foo.Foo()
+
+	foo.Foo1()
 }
